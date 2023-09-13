05a2b8201d5fefba7abb51b1106d01e0bcc39ce5
Iswenzz <alexisnardiello@gmail.com>
2023-09-13 18:16:59 +0200

    Port: libmariadb

--- ports/libmariadb/portfile.cmake
+++ ports/libmariadb/portfile.cmake
@@ -1,55 +1,73 @@
+if(EXISTS "${CURRENT_INSTALLED_DIR}/share/libmysql")
+    message(FATAL_ERROR "FATAL ERROR: libmysql and libmariadb are incompatible.")
+endif()
 
-if(EXISTS "${CURRENT_INSTALLED_DIR}/include/mysql/mysql.h")
-	message(FATAL_ERROR "FATAL ERROR: libmysql and libmariadb are incompatible.")
+if("openssl" IN_LIST FEATURES AND "schannel" IN_LIST FEATURES)
+    message(FATAL_ERROR "Only one SSL backend must be selected.")
 endif()
 
 vcpkg_from_github(
-	OUT_SOURCE_PATH SOURCE_PATH
-	REPO mariadb-corporation/mariadb-connector-c
-	REF c0ddc2c8cff30aebbedc5ae175e435c2c6fec646
-	SHA512 4130188036045ca3cd003378ea61c4420cf64e1d6ab77047bf95418a5323f1057951082fe8658ec3c57cb6d3d3f6326bad651cced0e9576fff923f6b8c2ad71e
-	HEAD_REF c0ddc2c8cff30aebbedc5ae175e435c2c6fec646
-	PATCHES mingw.patch
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO mariadb-corporation/mariadb-connector-c
+    REF 5e94e7c27ffad7e76665b1333a67975316b9c3c2
+    SHA512 0f740f88f64037990bf9d4593574b147ee02adb1fbbeb03c0dec745f0ee27d7cf03417dd09546ab70e16b4465622b8567864dbb243de0a3a7ffaebe313f7c231
+    HEAD_REF 5e94e7c27ffad7e76665b1333a67975316b9c3c2
+    PATCHES
+        md.patch
+        disable-test-build.patch
+        fix-InstallPath.patch
+        fix-iconv.patch
+        pkgconfig.patch
+        fix-openssl.patch
+        fix-CMakeLists.patch
+        zstd.patch
+		mingw.patch
 )
 
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        iconv            WITH_ICONV
+        mariadbclient    VCPKG_MARIADBCLIENT
+        zstd             WITH_ZSTD
+)
+
+if("openssl" IN_LIST FEATURES)
+    set(WITH_SSL OPENSSL)
+elseif("schannel" IN_LIST FEATURES)
+    set(WITH_SSL ON)
+else()
+    set(WITH_SSL OFF)
+endif()
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        -DINSTALL_INCLUDEDIR=include/mysql
+        -DINSTALL_LIBDIR=lib
+        -DINSTALL_PLUGINDIR=plugins/${PORT}
+        -DWITH_UNIT_TESTS=OFF
+        -DWITH_CURL=OFF
+        -DWITH_EXTERNAL_ZLIB=ON
+        -DWITH_SSL=${WITH_SSL}
+        -DREMOTEIO_PLUGIN_TYPE=OFF
+        -DAUTH_GSSAPI_PLUGIN_TYPE=OFF
+        -DWITH_ZSTD=${WITH_ZSTD}
+    MAYBE_UNUSED_VARIABLES
+        AUTH_GSSAPI_PLUGIN_TYPE
+)
+
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-libmariadb)
 
-file(INSTALL "${SOURCE_PATH}/README" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(INSTALL "${SOURCE_PATH}/COPYING.LIB" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/lib/pkgconfig")
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/lib/debug/pkgconfig")
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/bin/mariadb")
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/debug/bin/mariadb")
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/bin/plugin")
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/debug/bin/plugin")
-
-if(VCPKG_TARGET_IS_WINDOWS)
-	file(RENAME "${CURRENT_PACKAGES_DIR}/lib/mariadb/libmariadb.dll" "${CURRENT_PACKAGES_DIR}/bin/mariadb/libmariadb.dll")
-	file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/mariadb/libmariadb.dll" "${CURRENT_PACKAGES_DIR}/debug/bin/mariadb/libmariadb.dll")
-    file(RENAME "${CURRENT_PACKAGES_DIR}/lib/mariadb/plugin/caching_sha2_password.dll" "${CURRENT_PACKAGES_DIR}/bin/mariadb/plugin/caching_sha2_password.dll")
-    file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/mariadb/plugin/caching_sha2_password.dll" "${CURRENT_PACKAGES_DIR}/debug/bin/mariadb/plugin/caching_sha2_password.dll")
-    file(RENAME "${CURRENT_PACKAGES_DIR}/lib/mariadb/plugin/client_ed25519.dll" "${CURRENT_PACKAGES_DIR}/bin/mariadb/plugin/client_ed25519.dll")
-    file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/mariadb/plugin/client_ed25519.dll" "${CURRENT_PACKAGES_DIR}/debug/bin/mariadb/plugin/client_ed25519.dll")
-    file(RENAME "${CURRENT_PACKAGES_DIR}/lib/mariadb/plugin/dialog.dll" "${CURRENT_PACKAGES_DIR}/bin/mariadb/plugin/dialog.dll")
-    file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/mariadb/plugin/dialog.dll" "${CURRENT_PACKAGES_DIR}/debug/bin/mariadb/plugin/dialog.dll")
-    file(RENAME "${CURRENT_PACKAGES_DIR}/lib/mariadb/plugin/mysql_clear_password.dll" "${CURRENT_PACKAGES_DIR}/bin/mariadb/plugin/mysql_clear_password.dll")
-    file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/mariadb/plugin/mysql_clear_password.dll" "${CURRENT_PACKAGES_DIR}/debug/bin/mariadb/plugin/mysql_clear_password.dll")
-    file(RENAME "${CURRENT_PACKAGES_DIR}/lib/mariadb/plugin/pvio_shmem.dll" "${CURRENT_PACKAGES_DIR}/bin/mariadb/plugin/pvio_shmem.dll")
-    file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/mariadb/plugin/pvio_shmem.dll" "${CURRENT_PACKAGES_DIR}/debug/bin/mariadb/plugin/pvio_shmem.dll")
-    file(RENAME "${CURRENT_PACKAGES_DIR}/lib/mariadb/plugin/sha256_password.dll" "${CURRENT_PACKAGES_DIR}/bin/mariadb/plugin/sha256_password.dll")
-    file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/mariadb/plugin/sha256_password.dll" "${CURRENT_PACKAGES_DIR}/debug/bin/mariadb/plugin/sha256_password.dll")
+if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/libmariadb.pc" " -lmariadb" " -llibmariadb")
+    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+        vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/libmariadb.pc" " -lmariadb" " -llibmariadb")
+    endif()
 endif()
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-	file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
-	file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin")
-endif()
-
-file(RENAME "${CURRENT_PACKAGES_DIR}/debug/share/pkgconfig/zlib.pc" "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/zlib.pc")
-file(RENAME "${CURRENT_PACKAGES_DIR}/share/pkgconfig/zlib.pc" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/zlib.pc")
+vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(INSTALL "${SOURCE_PATH}/COPYING.LIB" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
